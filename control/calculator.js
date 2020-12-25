a544e6b4caafaa97509ccac64e7668e59388d30b
Edozie Valentine Nneke <valextreme2010@gmail.com>
2020-12-25 10:09:22 +0100

    Made changes to the css file paths and changes to the general html file

---
+++ control/calculator.js
@@ -0,0 +1,26 @@
+const top_display = document.getElementById('top-display');
+const calc_input = document.getElementById('calc-input');
+
+const calc_button = document.getElementsByClassName('calc-button');
+
+const cancel = document.getElementById('cancel');
+const divide = document.getElementById('divide');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const multiply = document.getElementById('multiply');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const subtract = document.getElementById('subtract');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const add = document.getElementById('add');
+const dot = document.getElementById('dot');
+const zero = document.getElementById('zero');
+const equal = document.getElementById('equal');
+
+cancel.addEventListener('click', () => {
+    
+})
