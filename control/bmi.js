59c8348cfd6080542d798eb0651d1fc9721783d4
Edozie Valentine Nneke <valextreme2010@gmail.com>
2021-01-09 18:48:42 +0100

    created a function to handle logic of BMI values

--- control/bmi.js
+++ control/bmi.js
@@ -21,19 +21,43 @@ metric[1].addEventListener('click', e => {
     heightInInches.style.display = 'inline-block';
 }, false);
 
+
+
 getBMI.addEventListener('click', e => {
     
+    e.preventDefault();
+
     if(metric[0].checked){
         let bmiKg;
         parseFloat(bmiKg = weight.value  / ((height.value/100) ** 2))
         display_bmi.innerHTML = bmiKg.toFixed(2);
+        outCome(bmiKg)
     }
     if(metric[1].checked){
         let bmiLbs, feetToinches;
         feetToinches = height.value * 12 + parseInt(heightInInches.value)
-        console.log(feetToinches)
         parseFloat(bmiLbs = (weight.value / (feetToinches ** 2)) * 703)
         display_bmi.innerHTML = bmiLbs.toFixed(2);
+        outCome(bmiLbs);
     }
-   e.preventDefault();
 }, false);
+
+
+function outCome(bmiValue){
+
+    if(bmiValue < 18.5){
+        // Underweight
+        console.log('Underweight')
+    }else if(bmiValue > 18.5 && bmiValue <= 24.9){
+        // Normal
+        console.log('Normal')
+    }else if(bmiValue >= 25.0 && bmiValue <= 29.9){
+        // Overweight
+        console.log('Overweight')
+    }else{
+        // Obese
+        console.log('Obese')
+    }
+}
+
+
