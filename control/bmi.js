c20647e1ad2e636f6ed1b2de95c6ca77e497a75c
Edozie Valentine Nneke <valextreme2010@gmail.com>
2021-01-23 08:51:29 +0100

    Added a button to clear user input

--- control/bmi.js
+++ control/bmi.js
@@ -1,66 +1,82 @@
 // DECLARE VARIABLES
-const metric = document.getElementsByName('metric');
-const getBMI = document.getElementById('bmi-button');
-const weight = document.getElementById('weight');
-const height = document.getElementById('height');
-const heightInInches = document.getElementById('heightInInches');
-const displayOutput = document.querySelector('#display-output');
-const display_bmi = document.getElementById('display-bmi');
+const metric = document.getElementsByName('metric')
+const weight = document.getElementById('weight')
+const height = document.getElementById('height')
+const heightInInches = document.getElementById('heightInInches')
+const getBMI = document.getElementById('bmi-button')
+const clearBtn = document.getElementById('clear-button')
+const displayOutput = document.querySelector('#display-output')
+const display_bmi = document.getElementById('display-bmi')
 
 // EVENTLISTENERS FOR RADIO BUTTONS
-metric[0].addEventListener('click', ()=> {
-    heightInInches.style.display = 'none';
+metric[0].addEventListener(
+  'click',
+  () => {
+    heightInInches.style.display = 'none'
     weight.setAttribute('placeholder', 'Kilograms')
     height.setAttribute('placeholder', 'Centimeters')
-}, false);
+  },
+  false
+)
 
-metric[1].addEventListener('click', () => {
+metric[1].addEventListener(
+  'click',
+  () => {
     weight.setAttribute('placeholder', 'Pounds')
     height.setAttribute('placeholder', 'Feet')
-    heightInInches.style.display = 'inline-block';
-}, false);
+    heightInInches.style.display = 'inline-block'
+  },
+  false
+)
 
 // EVENT LISTENER ON SUBMIT
-getBMI.addEventListener('click', e => {
-    
-    e.preventDefault();
+getBMI.addEventListener(
+  'click',
+  e => {
+    e.preventDefault()
 
-    if(metric[0].checked){
-        let bmiKg;
-        parseFloat(bmiKg = weight.value  / ((height.value/100) ** 2))
-        display_bmi.innerHTML = bmiKg.toFixed(2);
-        outCome(bmiKg)
+    if (metric[0].checked) {
+      let bmiKg
+      parseFloat((bmiKg = weight.value / (height.value / 100) ** 2))
+      display_bmi.innerHTML = bmiKg.toFixed(2)
+      outCome(bmiKg)
     }
-    if(metric[1].checked){
-        let bmiLbs, feetToinches;
-        feetToinches = height.value * 12 + parseInt(heightInInches.value)
-        parseFloat(bmiLbs = (weight.value / (feetToinches ** 2)) * 703)
-        display_bmi.innerHTML = bmiLbs.toFixed(2);
-        outCome(bmiLbs);
+    if (metric[1].checked) {
+      let bmiLbs, feetToinches
+      feetToinches = height.value * 12 + parseInt(heightInInches.value)
+      parseFloat((bmiLbs = (weight.value / feetToinches ** 2) * 703))
+      display_bmi.innerHTML = bmiLbs.toFixed(2)
+      outCome(bmiLbs)
     }
-}, false);
+  },
+  false
+)
 
 // FUNCTION TO HANDLE BMI LOGIC
-function outCome(bmiValue){
-
-    if(bmiValue < 18.5){
-        // Underweight
-        displayOutput.innerHTML = `Your BMI is: ${bmiValue.toFixed(2)}
-         and is below 18.5 which is a sign of being UNDERWEIGHT`;
-    }else if(bmiValue > 18.5 && bmiValue <= 24.9){
-        // Normal
-        displayOutput.innerHTML = `Your BMI is: ${bmiValue.toFixed(2)}
+function outCome(bmiValue) {
+  if (bmiValue < 18.5) {
+    // Underweight
+    displayOutput.innerHTML = `Your BMI is: ${bmiValue.toFixed(2)}
+         and is below 18.5 which is a sign of being UNDERWEIGHT`
+  } else if (bmiValue > 18.5 && bmiValue <= 24.9) {
+    // Normal
+    displayOutput.innerHTML = `Your BMI is: ${bmiValue.toFixed(2)}
          and it falls between the NORMAL range of 18.5 to 24.9`
-    }else if(bmiValue >= 25.0 && bmiValue <= 29.9){
-        // Overweight
-        displayOutput.innerHTML = `Your BMI is: ${bmiValue.toFixed(2)}
+  } else if (bmiValue >= 25.0 && bmiValue <= 29.9) {
+    // Overweight
+    displayOutput.innerHTML = `Your BMI is: ${bmiValue.toFixed(2)}
          and falls between the range of 25.0 and 29.9 which
-          is a sign of being OVERWEIGHT`;
-    }else{
-        // Obese
-        displayOutput.innerHTML = `Your BMI is: ${bmiValue.toFixed(2)}
-         and this above 29.9 and puts you at risk of OBESITY`;
-    }
+          is a sign of being OVERWEIGHT`
+  } else {
+    // Obese
+    displayOutput.innerHTML = `Your BMI is: ${bmiValue.toFixed(2)}
+         and this above 29.9 and puts you at risk of OBESITY`
+  }
 }
 
-
+// CLEAR ALL INPUTS AND DISPLAY
+clearBtn.addEventListener('click', () => {
+  displayOutput.innerHTML = ''
+  weight.value = ''
+  height.value = ''
+})
--- control/calculator.js
+++ control/calculator.js
@@ -1,87 +1,83 @@
+const topDisplay = document.getElementById('top-display')
+const calcInput = document.getElementById('calc-input')
 
-const topDisplay = document.getElementById('top-display');
-const calcInput = document.getElementById('calc-input');
+const calcButton = document.querySelectorAll('.calc-button')
 
-const calcButton = document.querySelectorAll('.calc-button');
+const cancel = document.getElementById('cancel')
+const divide = document.getElementById('divide')
+const seven = document.getElementById('seven')
+const eight = document.getElementById('eight')
+const nine = document.getElementById('nine')
+const multiply = document.getElementById('multiply')
+const four = document.getElementById('four')
+const five = document.getElementById('five')
+const six = document.getElementById('six')
+const subtract = document.getElementById('subtract')
+const one = document.getElementById('one')
+const two = document.getElementById('two')
+const three = document.getElementById('three')
+const add = document.getElementById('add')
+const dot = document.getElementById('dot')
+const zero = document.getElementById('zero')
+const equal = document.getElementById('equal')
 
-const cancel = document.getElementById('cancel');
-const divide = document.getElementById('divide');
-const seven = document.getElementById('seven');
-const eight = document.getElementById('eight');
-const nine = document.getElementById('nine');
-const multiply = document.getElementById('multiply');
-const four = document.getElementById('four');
-const five = document.getElementById('five');
-const six = document.getElementById('six');
-const subtract = document.getElementById('subtract');
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-const three = document.getElementById('three');
-const add = document.getElementById('add');
-const dot = document.getElementById('dot');
-const zero = document.getElementById('zero');
-const equal = document.getElementById('equal');
-
-console.log(calcInput);
+console.log(calcInput)
 // RESET CALULATOR INPUT
 cancel.addEventListener('click', () => {
-    calcInput.value = '0';
-    topDisplay.innerHTML = '';  
-});
+  calcInput.value = '0'
+  topDisplay.innerHTML = ''
+})
 
 // KEY INPUT
-for(let i = 0; i < calcButton.length; i++){
-    let specificButton = calcButton[i];
+for (let i = 0; i < calcButton.length; i++) {
+  let specificButton = calcButton[i]
 
-    specificButton.addEventListener('click', e => {
-        const nameOfSign = specificButton.name;
-/** Refractor to use chained if-else blocks to check all
- * possible outcomes
- */
-        switch (nameOfSign) {
-            case 'divide':
-                let divide = '';
-                if([' X ', ' + ', ' - '].indexOf(calcInput.value)){
-                    return;
-                }else{
-                divide = `${calcInput.value} /`;
-                topDisplay.innerHTML = divide;
-                }
-                break;
-
-            case 'multiply':
-                let multiply = '';
-                multiply = `${calcInput.value} X`;
-                topDisplay.innerHTML = multiply;
-                break;
-
-            case 'add':
-                let add = '';
-                add = `${calcInput.value} +`;
-                topDisplay.innerHTML = add;
-                break;
-
-            case 'subtract':
-                let subtract = '';
-                subtract = `${calcInput.value} -`;
-                topDisplay.innerHTML = subtract;
-                break;
-                
-            default:
-                break;
+  specificButton.addEventListener('click', e => {
+    const nameOfSign = specificButton.name
+    /** Refractor to use chained if-else blocks to check all
+     * possible outcomes
+     */
+    switch (nameOfSign) {
+      case 'divide':
+        let divide = ''
+        if ([' X ', ' + ', ' - '].indexOf(calcInput.value)) {
+          return
+        } else {
+          divide = `${calcInput.value} /`
+          topDisplay.innerHTML = divide
         }
-        // if(['divide', 'multipy', 'add', 'subtract'].indexOf(nameOfSign) > -1){
-        //     let sign = '';
-        //         sign = `${calcInput.value} / `;
-        //         topDisplay.innerHTML = sign; 
-        // }else{
-            if(calcInput.value === '0' && specificButton.value !== '.'){
-                calcInput.value = specificButton.value;
-            }else{
-                calcInput.value = calcInput.value + specificButton.value;    
-            }
-        
-    });
+        break
+
+      case 'multiply':
+        let multiply = ''
+        multiply = `${calcInput.value} X`
+        topDisplay.innerHTML = multiply
+        break
+
+      case 'add':
+        let add = ''
+        add = `${calcInput.value} +`
+        topDisplay.innerHTML = add
+        break
+
+      case 'subtract':
+        let subtract = ''
+        subtract = `${calcInput.value} -`
+        topDisplay.innerHTML = subtract
+        break
+
+      default:
+        break
+    }
+    // if(['divide', 'multipy', 'add', 'subtract'].indexOf(nameOfSign) > -1){
+    //     let sign = '';
+    //         sign = `${calcInput.value} / `;
+    //         topDisplay.innerHTML = sign;
+    // }else{
+    if (calcInput.value === '0' && specificButton.value !== '.') {
+      calcInput.value = specificButton.value
+    } else {
+      calcInput.value = calcInput.value + specificButton.value
+    }
+  })
 }
-
-
