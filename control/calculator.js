52703f03553df83951d86562be2f5db310f56fce
Edozie Valentine Nneke <valextreme2010@gmail.com>
2021-01-30 01:11:25 +0100

    I removed unused variable  from calculator.js and refractored actions for numbers

--- control/calculator.js
+++ control/calculator.js
@@ -3,21 +3,6 @@ const calcInput = document.getElementById('calc-input')
 const numbers = document.querySelectorAll('.num')
 const signs = document.querySelectorAll('.sign')
 const cancel = document.getElementById('cancel')
-const divide = document.getElementById('divide')
-const seven = document.getElementById('seven')
-const eight = document.getElementById('eight')
-const nine = document.getElementById('nine')
-const multiply = document.getElementById('multiply')
-const four = document.getElementById('four')
-const five = document.getElementById('five')
-const six = document.getElementById('six')
-const subtract = document.getElementById('subtract')
-const one = document.getElementById('one')
-const two = document.getElementById('two')
-const three = document.getElementById('three')
-const add = document.getElementById('add')
-const dot = document.getElementById('dot')
-const zero = document.getElementById('zero')
 const equal = document.getElementById('equal')
 
 // RESET CALULATOR INPUT
@@ -26,8 +11,19 @@ cancel.addEventListener('click', () => {
    topDisplay.innerHTML = ''
 })
 
-// KEY ACTIONS
+// KEY ACTIONS FOR NUMBERS
 for (let n = 0; n < numbers.length; n++) {
    let nums = numbers[n]
-   nums.addEventListener('click', () => {})
+   let digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+   nums.addEventListener('click', () => {
+      if (nums.value === '0' && calcInput.value === '0') {
+         calcInput.value = ''
+         calcInput.value = calcInput.value + nums.value
+      } else if (digits.indexOf(nums.value) > -1 && calcInput.value === '0') {
+         calcInput.value = ''
+         calcInput.value = calcInput.value + nums.value
+      } else {
+         calcInput.value = calcInput.value + nums.value
+      }
+   })
 }
